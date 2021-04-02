a0a4055f0afab93bf6d3179ac47edb193c77bc88
CailleauThierry <thierrycailleau@yahoo.fr>
2021-04-02 13:53:58 +0200

    adding Divide_by_exception.py

---
+++ Functions/LocalScope.py
@@ -0,0 +1,8 @@
+def spam():
+    eggs = 99
+
+spam()
+#this print will error out as eggs is only defined in the local scope
+print(eggs)
+
+
---
+++ Functions/MyFunction.py
@@ -0,0 +1,7 @@
+def hello():
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Oh, hello there!!')
+hello()
+hello()
+hello()
